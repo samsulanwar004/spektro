3800a6bab94228bf31f4b14d9231d0e6cd74fe99
samrock <sam.aan46@gmail.com>
2022-06-03 13:58:26 +0700

    percobaan

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const webpack = require( 'webpack' );
 
 module.exports = ( env, options ) => {
 	return {
-		
+		vendor: [
+	        'xlsx',
+	        'file-saver'
+		],
 		node: {fs: 'empty'},
 		externals: [
 		    {'./cptable': 'var cptable'},
